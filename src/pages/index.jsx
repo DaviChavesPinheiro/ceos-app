79e84eab24b925156b9b5cc7969364e986e17e8b
Davi <davicpinheiro123@hotmail.com>
2020-12-05 18:05:35 -0300

    Inital Setup: Styled Components

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CEOS - Empresa Júnior</title>
+      </Head>
+
+      <main>
+        <p>Main</p>
+      </main>
+
+      <footer >
+        <p>Footer</p>
+      </footer>
+    </div>
+  )
+}
