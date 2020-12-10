ed0237eea036b3721c93a98b20f15aae3e6d9781
Davi <davicpinheiro123@hotmail.com>
2020-12-09 23:12:34 -0300

    Footer Refactored

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,49 @@
-import { Container } from "./styles";
+import { Container, Content, CopyRightContainer } from "./styles";
 import Link from "next/link";
+import Image from "next/image";
+import RoundButton from "../RoundButton";
+import { FiFacebook, FiInstagram, FiLinkedin, FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
 
   return (
     <Container>
-      <ul>
-        <li><Link href="/"><a>Início</a></Link></li>
-        <li><Link href="/about"><a>Sobre nós</a></Link></li>
-        <li><Link href="/portfolio"><a>Portfólio</a></Link></li>
-        <li><Link href="/contact"><a>Fale Conosco</a></Link></li>
-      </ul>
+      <Content>
+        <div>
+          <div className="content">
+            <Image src="/CEOS.svg" layout="intrinsic" height="15" width="30"/>
+            <p>CEOS - Transformando código em história</p>
+          </div>
+        </div>
+        <div>
+          <h2>Links</h2>
+          <ul>
+            <li><Link href="/"><a>Início</a></Link></li>
+            <li><Link href="/about"><a>Sobre nós</a></Link></li>
+            <li><Link href="/portfolio"><a>Portfólio</a></Link></li>
+            <li><Link href="/contact"><a>Fale Conosco</a></Link></li>
+          </ul>
+        </div>
+        <div>
+          <h2>Contato</h2>
+          <ul>
+            <li>+55 85 88325-6564</li>
+            <li>adm@ceos.com.br</li>
+            <li>Fortaleza, CE</li>
+          </ul>
+        </div>
+        <div>
+          <RoundButton icon={FiFacebook} color={"#2961fe"} fill={"#2961fe"}></RoundButton>
+          <RoundButton icon={FiTwitter} color={"#2961fe"} fill={"#2961fe"}></RoundButton>
+          <RoundButton icon={FiLinkedin} color={"#2961fe"} fill={"#2961fe"}></RoundButton>
+          <RoundButton icon={FiInstagram} color={"#2961fe"}></RoundButton>
+        </div>
+      </Content>
+      <CopyRightContainer>
+        <p>Copyright All Right reserved.</p>
+      </CopyRightContainer>
     </Container>
+
   )
 }
 
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,18 +1,57 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
+export const Container = styled.footer`
   position: absolute;
   z-index: 10;
   width: 100%;
 
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-end;
+  flex-direction: column;
+  align-items: stretch;
 
-  padding: 30px 0px 10px 0px;
+  padding-top: 70px;
 
   background-color: #f4f7fc;
+
+`
+
+export const Content = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+
+  padding: 0px 10px;
+  padding-bottom: 60px;
+
+  div:nth-child(1) {
+    flex: 2;
+    .content {
+      margin-left: 120px;
+    }
+  }
+  div:nth-child(2) {
+    flex: 1;
+  }
+  div:nth-child(3) {
+    flex: 1;
+  }
+  div:nth-child(4) {
+    flex: 2;
+    display: flex;
+    justify-content: flex-end;
+
+    button {
+      margin-right: 15px;
+    }
+    button:last-child {
+      margin-right: 80px;
+    }
+  }
+
+  h2 {
+    margin-bottom: 10px;
+  }
 
   ul {
     display: flex;
@@ -20,6 +59,37 @@ export const Container = styled.header`
 
     list-style-type: none;
 
+    li {
+      margin-bottom: 10px;
+    }
+  }
+
+  p, li, a {
+    font-size: 14px;
+    line-height: 25px;
+    color: #909090;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`
+
+export const CopyRightContainer = styled.div`
+  border-top: 1px solid #dedede;
+
+  width: 100%;
+  padding: 10px 5px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 14px;
+    line-height: 25px;
+    color: #909090;
   }
 
 `
